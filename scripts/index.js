200fb9bda380e85257c79fd3ebbf80f3574ca0cc
admin <andryuha_nikolaev@mail.ru>
2022-04-07 16:32:23 +0300

    шестая проектная без валидатора

--- scripts/index.js
+++ scripts/index.js
@@ -1,104 +1,94 @@
-const popupElement = document.querySelector('#popup-edit');
-const editButton = document.querySelector('#edit-button');
-const closeButton = popupElement.querySelector('#edit-close-button');
-const formElement = document.querySelector('#edit-form');
-const nameInput = formElement.querySelector('#input__name');
-const jobInput = formElement.querySelector('#input__about');
-const buttonSave = formElement.querySelector('#edit-button-save');
-const profileName = document.querySelector('.profile__name');
-const profileCaption = document.querySelector('.profile__caption');
+const popups = document.querySelectorAll('.popup'); //все попапы
+const closeButton = document.querySelector('.popup__close-button'); //общая кнопка закрытия для всех попапов
 
-const addPopupElement = document.querySelector('#popup-add'); //подключаем попап добавления фото
-const addButton = document.querySelector('#add-button'); //добавляем кнопку открытия попапа
-const addFormElement = document.querySelector('#add-form'); //добавляем форму попапа
-const namePhoto = addFormElement.querySelector('#photo-name'); //добавялем инпут названия фото
-const linkPhoto = addFormElement.querySelector('#photo-link'); //добавляем инпут ссылки на фото
-const addCloseButton = addPopupElement.querySelector('#add-close-button'); //добавляем кнопку закрытия попапа
-const addButtonSave = addFormElement.querySelector('#add-button-save'); //добавляем кнопку сохранения попапа
+const editPopupElement = document.querySelector('#popup-edit'); //попап редактирования профиля
+const editButton = document.querySelector('#edit-button'); //кнопка открытия попапа редактирования
+const editFormElement = document.querySelector('#edit-form'); //форма попапа редактирования профиля
+const nameInput = editFormElement.querySelector('#input__name'); //инпут имени
+const jobInput = editFormElement.querySelector('#input__about'); //инпут описания
+const editButtonSave = editFormElement.querySelector('#edit-button-save'); //кнопка сохранения попапа редактирования профиля
+const profileName = document.querySelector('.profile__name'); //имя профиля на странице
+const profileCaption = document.querySelector('.profile__caption'); //описание профиля на странице
+
+const addPopupElement = document.querySelector('#popup-add'); //попап добавления фото
+const addButton = document.querySelector('#add-button'); //кнопка открытия попапа добавления фото
+const addFormElement = document.querySelector('#add-form'); //форма попапа добавления фото
+const namePhoto = addFormElement.querySelector('#photo-name'); //инпут названия фото
+const linkPhoto = addFormElement.querySelector('#photo-link'); //инпут ссылки на фото
+const addButtonSave = addFormElement.querySelector('#add-button-save'); //кнопка сохранения попапа добавления фото
 const photosContainer = document.querySelector('.elements__list'); //подключили список элементов
 
-const template = document.querySelector('#template-list-item');
+const template = document.querySelector('#template-list-item');//подключили темплейт
 
-const imagePopupElement = document.querySelector('.popup_view-image'); //подкелючаем попап картинки
-const imagePopupClose = imagePopupElement.querySelector('#image-close-button'); //подключаем кнопку закрытия попапа
-const imagePopupImage = imagePopupElement.querySelector('.popup__image'); //подключаем изображение попапа
-const imagePopupDescription = imagePopupElement.querySelector('.popup__description'); //подключаем описание картинки попапа
+const imagePopupElement = document.querySelector('.popup_view-image'); //попап картинки
+const imagePopupImage = imagePopupElement.querySelector('.popup__image'); //изображение попапа картинки
+const imagePopupDescription = imagePopupElement.querySelector('.popup__description'); //описание картинки попапа
+
+//функция закрытия попапов на esc
+const EscPress = (evt) => {
+  if (evt.key === 'Escape') {
+    const popup = document.querySelector('.popup_opened');
+    closePopup(popup);
+  }
+};
 
 //функция открытия попапа
-function openPopup(popup) {
+const openPopup = (popup) => {
   popup.classList.add('popup_opened');
-}
+  document.addEventListener('keyup', EscPress);
+};
 
 //функция закрытия попапа
-function closePopup(popup) {
+const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
-}
+  document.removeEventListener('keyup', EscPress);
+};
 
-//кнопка октрытия попапа редактирования профиля
-editButton.addEventListener('click', () => {
+//функция попапа редактирования профиля
+const openEditPopup = () => {
   nameInput.value = profileName.textContent;
   jobInput.value = profileCaption.textContent;
-  openPopup(popupElement);
-})
+  // validationPopupEdit.resetPopupForm();
+  openPopup(editPopupElement);
+};
 
-//кнопка закрытия попапа редактирования профиля
-closeButton.addEventListener('click', () => {
-  closePopup(popupElement);
-})
+//функция попапа добавления фото
+const openAddPopup = () => {
+  addFormElement.reset();
+  // validationPopupAdd.resetPopupForm();
+  openPopup(addPopupElement);
+};
 
+//кнопка октрытия попапа редактирования профиля
+editButton.addEventListener('click', openEditPopup);
+
+//кнопка открытия попапа добавления фото
+addButton.addEventListener('click', openAddPopup);
+
+//общая функция для закрытия выбранного попапа
+popups.forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_opened')) {
+      closePopup(popup);
+    }
+    if (evt.target.classList.contains('popup__close-button')) {
+      closePopup(popup);
+    }
+  })
+});
+
+//функция изменения данных профиля
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileCaption.textContent = jobInput.value;
-  closePopup(popupElement);
+  closePopup(editPopupElement);
 }
 
-formElement.addEventListener('submit', handleProfileFormSubmit);
+//обработчик изменяет данные профиля по нажатию кнопки сохранить
+editFormElement.addEventListener('submit', handleProfileFormSubmit);
 
-//массив фото
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-//кнопка открытия попапа добавления фото
-addButton.addEventListener('click', () => {
-  openPopup(addPopupElement);
-});
-
-//кнопка закрытия попапа добавления фото
-addCloseButton.addEventListener('click', () => {
-  closePopup(addPopupElement)
-});
-
-//кнопка закрытия попапа картинки
-imagePopupClose.addEventListener('click', () => {
-  closePopup(imagePopupElement)
-});
-
-//функция сгенерирует html элемент и вернет его
+//функция добавления карточки. сгенерирует html элемент и вернет его
 const createPhoto = (photoCard) => {
   const card = template.content.querySelector('.elements__list-item').cloneNode(true);
   card.querySelector('.elements__text').textContent = photoCard.name;
@@ -126,11 +116,12 @@ const createPhoto = (photoCard) => {
   return card;
 }
 
-
+//функция принимает photoCard, создает строку с использованием значений photoCard и добавляет ее в начало списка elements__list
 const renderPhoto = (photoCard) => {
   photosContainer.prepend(createPhoto(photoCard));
-} //функция принимает photoCard, создает строку с использованием значений photoCard и добавляет ее в начало списка elements__list
+}
 
+//функция добавляет фото
 const addPhoto = (event) => {
   event.preventDefault(); //запрещаем выполнение события по умолчанию, чтобы при отправе страница не перезагружалась
   let photoCard = { }; //создаем объект
@@ -142,11 +133,13 @@ const addPhoto = (event) => {
   linkPhoto.value = ''; //очищает инпут
 }
 
-
+//перебираем массив и выводим результат в новый отдельный массив
 const elements = initialCards.map(photoCard => {
   return createPhoto(photoCard);
-}); //перебираем массив и выводим результат в новый отдельный массив
+});
 
 photosContainer.append(...elements);
-addFormElement.addEventListener('submit', addPhoto); //вешаем обработчик события на форму добавления новой карточки. При нажатии на Создать, выполнятся функция addPhoto
+
+//вешаем обработчик события на форму добавления новой карточки. При нажатии на Создать, выполнятся функция addPhoto
+addFormElement.addEventListener('submit', addPhoto);
 
